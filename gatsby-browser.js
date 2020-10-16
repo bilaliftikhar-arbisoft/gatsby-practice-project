3fc70e63ff9a724127970528b4510e6d719aacf7
bilal iftikhar <bilal.iftikhar@arbisoft.com>
2020-10-16 12:28:40 +0500

    :recycle: wrap providers

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,3 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
+import { Providers } from './src/providers';
 
-// You can delete this file if you're not using it
+export const wrapRootElement = Providers;
--- src/pages/app.js
+++ src/pages/app.js
@@ -7,24 +7,16 @@ import Profile from '../components/profile';
 import Layout from '../components/layout';
 import Login from '../components/login';
 import PrivateRoute from '../routes';
-import Providers from '../providers';
 
-
-const App = () => {
-  return (
-    <Providers layout={
-      () =>
-        <Layout>
-          <Router basepath="/app">
-            <PrivateRoute path="/profile" component={Profile} />
-            <PrivateRoute path="/profile/:id" component={Profile} />
-            <PrivateRoute path="/login" component={Login} />
-            <PrivateRoute path="/users" component={UserList} />
-            <NotFound path="*" />
-          </Router>
-        </Layout>
-    } />
-  );
-};
+const App = () =>
+  <Layout>
+    <Router basepath="/app">
+      <PrivateRoute path="/profile" component={Profile} />
+      <PrivateRoute path="/profile/:id" component={Profile} />
+      <PrivateRoute path="/login" component={Login} />
+      <PrivateRoute path="/users" component={UserList} />
+      <NotFound path="*" />
+    </Router>
+  </Layout>;
 
 export default App;
